e3e9bc13173baa58f3bac737b2be8f6142a843c1
Brian Hong <brian@bhong.dev>
2022-01-17 19:30:02 -0500

    Drawing a simple rectangle
    Also defined some colors and set framerate

--- Pong.py
+++ Pong.py
@@ -4,6 +4,15 @@ pygame.init()
 # Create screen
 screen_size = (800, 500)
 screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption("Pong")
+
+# Get pygame clock
+clock = pygame.time.Clock()
+
+# Define some colors
+BLUE = (25, 151, 224)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 running = True
 while running:
@@ -14,12 +23,20 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    # Process user input
+    # TODO
+
     # Draw things onto the screen
     # TODO
+    screen.fill(BLACK)
+
+    # Draw the (left) paddle
+    pygame.draw.rect(screen, WHITE, [30, 50, 30, 70])
 
     # Update screen
     pygame.display.flip()
 
     # Set framerate
-    # TODO
+    clock.tick(60)
 
+pygame.quit()
