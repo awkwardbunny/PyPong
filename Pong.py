bb7d1b2e520c20e12973013fa9146d25c0afee0e
Brian Hong <brian@bhong.dev>
2022-01-24 19:30:00 -0500

    Added right paddle (with same controls)

--- Pong.py
+++ Pong.py
@@ -14,9 +14,11 @@ clock = pygame.time.Clock()
 BLUE = (25, 151, 224)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+GRAY = (200, 200, 200)
 
-paddle_size = [30, 70]
+paddle_size = [20, 90]
 left_paddle = [30, 50]
+right_paddle = [750, 50]
 
 going_up = False
 going_down = False
@@ -26,34 +28,40 @@ while running:
 
     # Check events
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: # if user clicked the quit button
-            running = False # end loop
-        elif event.type == pygame.KEYDOWN: # this will run when a key is pressed
+        if event.type == pygame.QUIT:  # if user clicked the quit button
+            running = False  # end loop
+        elif event.type == pygame.KEYDOWN:  # this will run when a key is pressed
             # Check WHICH key was pressed
-            if event.key == pygame.K_UP: # if the UP key was pressed
+            if event.key == pygame.K_UP:  # if the UP key was pressed
                 going_up = True
                 going_down = False
-            elif event.key == pygame.K_DOWN: # if the DOWN key was pressed
+            elif event.key == pygame.K_DOWN:  # if the DOWN key was pressed
                 going_up = False
                 going_down = True
-        elif event.type == pygame.KEYUP: # this will run when a key is released
-            if event.key == pygame.K_UP: # if the UP key was released
-                going_up = False # stop moving up
-            if event.key == pygame.K_DOWN: # if the DOWN key was released
-                going_down = False # stop moving down
+        elif event.type == pygame.KEYUP:  # this will run when a key is released
+            if event.key == pygame.K_UP:  # if the UP key was released
+                going_up = False  # stop moving up
+            if event.key == pygame.K_DOWN:  # if the DOWN key was released
+                going_down = False  # stop moving down
 
     if going_up:
-        if left_paddle[1] > 0: # check if at the top of screen
+        if right_paddle[1] > 0:  # check if at the top of screen
+            right_paddle[1] -= 4
+        if left_paddle[1] > 0:  # check if at the top of screen
             left_paddle[1] -= 4
     elif going_down:
-        if left_paddle[1]+paddle_size[1] < 500: # check if (the bottom left corner) is at bottom of screen
+        if right_paddle[1] + paddle_size[1] < 500:  # check if (the bottom left corner) is at bottom of screen
+            right_paddle[1] += 4
+        if left_paddle[1] + paddle_size[1] < 500:  # check if (the bottom left corner) is at bottom of screen
             left_paddle[1] += 4
 
     # Draw things onto the screen
-    ## Clear screen to black
+    # Clear screen to black
     screen.fill(BLACK)
-    ## Draw the (left) paddle
-    pygame.draw.rect(screen, WHITE, [left_paddle[0], left_paddle[1], paddle_size[0], paddle_size[1]])
+    # Draw the (right) paddle
+    pygame.draw.rect(screen, GRAY, [right_paddle[0], right_paddle[1], paddle_size[0], paddle_size[1]])
+    # Draw the (left) paddle
+    pygame.draw.rect(screen, GRAY, [left_paddle[0], left_paddle[1], paddle_size[0], paddle_size[1]])
 
     # Update screen
     pygame.display.flip()
